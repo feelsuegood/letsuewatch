cd2f1aeb3ecd94c549c0d5c5beee0645c332004a
feelsuegood <suaj2021@gmail.com>
2023-09-02 20:25:07 +1000

    1

---
+++ README.md
@@ -0,0 +1,18 @@
+# Wetube Reloaded
+
+Global routers
+/ -> Home
+cute url, exception of rules(logically '/users/login' is more suitable)
+/join -> Join
+/login -> Login
+/search -> Search
+
+/user/:id -> See user
+/users/logout -> Log Out
+/users/edit -> Edit My Profile
+/users/remove -> Remove My Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
---
+++ src/controllers/videoController.js
@@ -0,0 +1,13 @@
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => {
+  res.render("watch", { pageTitle: "Watch" });
+};
+export const edit = (req, res) => {
+  res.render("Edit");
+};
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  res.send("Delete");
+};
