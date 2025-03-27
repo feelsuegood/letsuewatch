379033b1be78fe1885f780c21fe446eec9a6f7a2
feelsuegood <suaj2021@gmail.com>
2025-03-27 14:06:53 +1000

    #13.3 Video Recorder

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,40 @@
 const startBtn = document.getElementById('startBtn');
+const video = document.getElementById('preview');
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = 'Start recording';
+  startBtn.removeEventListener('click', handleStop);
+  startBtn.addEventListener('click', handleStart);
+};
+const handleStart = () => {
+  startBtn.innerText = 'Stop recording';
+  startBtn.removeEventListener('click', handleStart);
+  startBtn.addEventListener('click', handleStop);
+  const recorder = new MediaRecorder(stream);
+  console.log(recorder);
+  recorder.ondataavailable = (e) => {
+    console.log(`recording done`);
+    console.log(e);
+    console.log(e.data);
+  };
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
+
+init();
 
 startBtn.addEventListener('click', handleStart);
