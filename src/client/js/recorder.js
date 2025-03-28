58f69a08078fe242f0684e9ef8a0994f8a2f9777
feelsuegood <suaj2021@gmail.com>
2025-03-28 14:30:35 +1000

    #14.4 thumbnail image

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,5 +1,5 @@
 import { FFmpeg } from '@ffmpeg/ffmpeg';
-import { fetchFile, toBlobURL } from '@ffmpeg/util';
+import { fetchFile } from '@ffmpeg/util';
 
 const startBtn = document.getElementById('startBtn');
 const video = document.getElementById('preview');
@@ -15,15 +15,43 @@ const handleDownload = async () => {
   await ffmpeg.load();
   await ffmpeg.writeFile('recording.webm', await fetchFile(videoFile));
   await ffmpeg.exec(['-i', 'recording.webm', '-r', '60', 'output.mp4']);
+  await ffmpeg.exec([
+    '-i',
+    'recording.webm',
+    '-ss',
+    '00:00:01',
+    '-frames:v',
+    '1',
+    'thumbnail.jpg',
+  ]);
   const mp4File = await ffmpeg.readFile('output.mp4');
+  const thumbFile = await ffmpeg.readFile('thumbnail.jpg');
+
   const mp4Blob = new Blob([mp4File.buffer], { type: 'video/mp4' });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: 'image/jpg' });
+
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
   const a = document.createElement('a');
   a.href = mp4Url;
   a.download = 'MyRecording.mp4';
   document.body.appendChild(a);
   a.click();
+
+  const thumbA = document.createElement('a');
+  thumbA.href = thumbUrl;
+  thumbA.download = 'MyThumbnail.jpg';
+  document.body.appendChild(thumbA);
+  thumbA.click();
+
+  await ffmpeg.deleteFile('output.mp4');
+  await ffmpeg.deleteFile('thumbnail.jpg');
+  await ffmpeg.deleteFile('recording.webm');
+
+  URL.revokeObjectURL(mp4Blob);
+  URL.revokeObjectURL(thumbBlob);
+  URL.revokeObjectURL(videoFile);
 };
 
 const handleStop = () => {
