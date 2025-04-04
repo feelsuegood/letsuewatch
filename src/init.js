28fe080a989797c38a493dc6312e32b5e20f5a1f
feelsuegood <suaj2021@gmail.com>
2025-04-04 12:53:33 +1000

    fix: listen on 0.0.0.0 for Fly.io

--- src/init.js
+++ src/init.js
@@ -5,8 +5,8 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
-app.listen(PORT, handleListening);
+app.listen(PORT, "0.0.0.0", handleListening);
